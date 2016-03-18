de1bef1470d7be7a65f016654f27ff3f1c8c42c4
lijippy@163.com <lijippy@163.com>
2016-03-18 17:56:03 +0800

    haha

---
+++ example/values.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	/* string plus*/
+	fmt.Println("go" + "lang")
+	fmt.Println("1+1 =", 1+1)
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+
+	// Booleans, with boolean operators as you'd expect.
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
